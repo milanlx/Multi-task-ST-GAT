d9e226c2b0a3917418f1b90b8135f74c12d2bef2
Xuan Li <kkkacfly@hotmail.com>
2021-05-06 12:24:52 -0400

    Add files via upload

--- batchGenerator.py
+++ batchGenerator.py
@@ -15,4 +15,22 @@ class STGraphDateset(Dataset):
         return _x, _y
 
 
+class TotalSTGraphDateset(Dataset):
+    def __init__(self, x_bus, x_inrix, x_ele, x_weather, y):
+        self.x_bus = x_bus
+        self.x_inrix = x_inrix
+        self.x_ele = x_ele
+        self.x_weather = x_weather
+        self.y = y
+
+    def __len__(self):
+        return self.y.shape[0]
+
+    def __getitem__(self, index):
+        _x_bus = self.x_bus[index]
+        _x_inrix = self.x_inrix[index]
+        _x_ele = self.x_ele[index]
+        _x_weather = self.x_weather[index]
+        _y = self.y[index]
+        return _x_bus, _x_inrix, _x_ele, _x_weather, _y
 
